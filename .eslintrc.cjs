37bb84c8c856ed41598db1feae24442604bb2d4c
charleslewisdev <charleslewisdev@users.noreply.github.com>
2026-06-19 11:23:24 -0700

    chore: scaffold pnpm monorepo with typescript, vitest, eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module', ecmaFeatures: { jsx: true } },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { node: true, browser: true, es2022: true },
+  ignorePatterns: ['dist', 'node_modules', '*.config.*'],
+};
